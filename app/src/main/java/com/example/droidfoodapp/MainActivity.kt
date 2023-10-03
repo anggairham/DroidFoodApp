d835d37dd2adb342ac4f3191c29a8ffedb92f167
Angga Irham Stianto <angga@usm.ac.id>
2023-10-03 15:41:42 +0700

    update

--- app/src/main/java/com/example/droidfoodapp/MainActivity.kt
+++ app/src/main/java/com/example/droidfoodapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.droidfoodapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<ConstraintLayout>(R.id.btn_start)
+        startBtn.setOnClickListener {
+            val moveIntent = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(moveIntent)
+        }
     }
 }
