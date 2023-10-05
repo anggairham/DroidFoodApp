0faab45e4707237796ac4c5e8cc01492039b8581
Angga Irham Stianto <angga@usm.ac.id>
2023-10-05 10:55:18 +0700

    update 1.59.29

---
+++ app/src/main/java/com/example/droidfoodapp/SampleDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.droidfoodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_detail);
+    }
+}
